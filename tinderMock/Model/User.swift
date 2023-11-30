da930b292df6922c452a7eb2b8eab6eec9253569
Haruko Okada <hokada@uiowa.edu>
2023-11-30 14:45:45 -0600

    feature: created user model (might add more fields later)

---
+++ tinderMock/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  tinderMock
+//
+//  Created by Haruko Okada on 11/30/23.
+//
+
+import Foundation
+
+struct User: Identifiable {
+    let id: String
+    let email: String
+    let first_name: String
+    let last_name: String
+    let dob: Date
+    let gender: Gender
+    let sexuality: Sexuality
+}
