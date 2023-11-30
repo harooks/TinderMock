87bc341da450a00e0b3bef55b62084194a112b9b
Haruko Okada <hokada@uiowa.edu>
2023-11-30 15:32:05 -0600

    feature: implemented createUser

--- tinderMock/Authentication/AuthViewModel.swift
+++ tinderMock/Authentication/AuthViewModel.swift
@@ -7,13 +7,16 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
+@MainActor
 class AuthViewModel: ObservableObject {
     @Published var userSession: FirebaseAuth.User?
     @Published var currentUser: User?
     
     init() {
-        
+        self.userSession = Auth.auth().currentUser
     }
     
     func signIn(withEmail email: String, password: String) async throws {
@@ -22,6 +25,15 @@ class AuthViewModel: ObservableObject {
     
     func createUser(withEmail email: String, password: String, first_name: String, last_name: String, dob: Date, gender: Gender, sexuality: Sexuality) async throws {
         print("Create user")
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            let user = User(id: result.user.uid, email: email, first_name: first_name, last_name: last_name, dob: dob, gender: gender, sexuality: sexuality)
+            let encodedUser = try Firestore.Encoder().encode(user)
+            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+        } catch {
+            print("Failed to create user: \(error.localizedDescription)")
+        }
     }
     
     func signOut() {
@@ -29,7 +41,7 @@ class AuthViewModel: ObservableObject {
     }
     
     func fetchUser() async {
-        
+
     }
 }
 
