d3e049bae8c2e41eee7d8d16c64de8ce4867f2a5
Haruko Okada <hokada@uiowa.edu>
2023-11-30 09:58:24 -0600

    initial commit

---
+++ tinderMock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tinderMock
+//
+//  Created by Haruko Okada on 11/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tinderMock/tinderMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinderMockApp.swift
+//  tinderMock
+//
+//  Created by Haruko Okada on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct tinderMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
