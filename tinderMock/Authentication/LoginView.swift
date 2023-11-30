737eae55d2378166c6c5a3f77cbea555431b4432
Haruko Okada <hokada@uiowa.edu>
2023-11-30 12:26:13 -0600

    feature: created login view

---
+++ tinderMock/Authentication/LoginView.swift
@@ -0,0 +1,55 @@
+//
+//  LoginView.swift
+//  tinderMock
+//
+//  Created by Haruko Okada on 11/30/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    var body: some View {
+        NavigationStack {
+            
+            //login form
+            VStack(spacing: 20) {
+                InputView(text: $email, title: "Email", placeholder: "Enter email")
+                InputView(text: $password, title: "Password", placeholder: "Enter password", isSecureField: true)
+            }
+            .padding(.horizontal)
+            .padding(.top,20)
+            
+            //sign in button
+            Button {
+                print("logging in")
+            } label: {
+                Text("Sign in")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 40)
+            }
+            .background(Color(.systemBlue))
+            .cornerRadius(10)
+            .padding(.top, 24)
+                            
+            //sign up button
+            NavigationLink {
+                
+            } label: {
+                VStack(spacing: 5) {
+                    Text("Don't have an account?")
+                    Text("Sign up")
+                        .fontWeight(.bold)
+                }
+            }
+            .padding(.top, 40)
+
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
